a8d8c58104aed78cd4852f108531a1b576cd832d
Bryant Bourgeois <bryant.bourgeois@dismylab.com>
2023-08-29 21:02:30 -0600

    Add capability to draw square cells on the canvas

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from window import Window
+from window import Window, Line, Point, Cell
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(125, 125)
+    p2 = Point(375, 375)
+    c1 = Cell(p1.x, p1.y, p2.x, p2.y, win)
+    p3 = Point(250, 250)
+    p4 = Point(500, 500)
+    c2 = Cell(p3.x, p3.y, p4.x, p4.y, win)
+    c1.draw()
+    c2.draw()
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -21,3 +21,58 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+        canvas.pack()
+
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self):
+        if self.has_left_wall:
+            p1 = Point(self._x1, self._y1)
+            p2 = Point(self._x1, self._y2)
+            line = Line(p1, p2)
+            self._win.draw_line(line, 'purple')
+        if self.has_bottom_wall:
+            p1 = Point(self._x1, self._y2)
+            p2 = Point(self._x2, self._y2)
+            line = Line(p1, p2)
+            self._win.draw_line(line, 'purple')
+        if self.has_right_wall:
+            p1 = Point(self._x2, self._y1)
+            p2 = Point(self._x2, self._y2)
+            line = Line(p1, p2)
+            self._win.draw_line(line, 'purple')
+        if self.has_top_wall:
+            p1 = Point(self._x1, self._y1)
+            p2 = Point(self._x2, self._y1)
+            line = Line(p1, p2)
+            self._win.draw_line(line, 'purple')
