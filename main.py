5e3b96f8cdfe8029084da9d6cc328612ac6d05cc
Bryant Bourgeois <bryant.bourgeois@dismylab.com>
2023-09-03 16:57:00 -0600

    Back to the point of drawing a maze

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from maze import Maze
+from window import Window
 
 
 def main():
+    win = Window(600, 800)
+    maze = Maze(
+        50,
+        50,
+        20,
+        20,
+        20,
+        20,
+        win,
+    )
+    win.wait_for_close()
 
 
 if __name__ == "__main__":
